d4932fd300234ddbe0acf9200cbbd3e3c698c27e
Artem Bei <artem.bei@ua.glomex.com>
2018-03-20 15:41:45 +0200

    validate npm version

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -66,6 +66,33 @@ switch (script) {
     });
     break;
   }
+  case 'validate': {
+    const result = spawn.sync(
+      'node',
+      nodeArgs
+        .concat(require.resolve('../scripts/' + script)),
+      { stdio: 'inherit' }
+    );
+    console.log(result.status)
+    if (result.signal) {
+      // if (result.signal === 'SIGKILL') {
+      //   console.log(
+      //     'The build failed because the process exited too early. ' +
+      //       'This probably means the system ran out of memory or someone called ' +
+      //       '`kill -9` on the process.'
+      //   );
+      // } else if (result.signal === 'SIGTERM') {
+      //   console.log(
+      //     'The build failed because the process exited too early. ' +
+      //       'Someone might have called `kill` or `killall`, or the system could ' +
+      //       'be shutting down.'
+      //   );
+      // }
+      // process.exit(1);
+    }
+    process.exit(result.status);
+    break;
+  }
   default:
     console.log('Unknown script "' + script + '".');
     console.log('Perhaps you need to update react-scripts?');
